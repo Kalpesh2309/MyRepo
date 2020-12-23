9c8194261abd62659907d3916a5e827c8f946bdf
Kalpesh Ukey <kalpesh.ukey@hcl.com>
2020-12-23 11:01:07 +0530

    new Changes

--- src/app/books/book-list/book-item/book-item.component.ts
+++ src/app/books/book-list/book-item/book-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Book } from 'src/app/shared/Book.model';
+import { Book } from '../../../shared/Book.model';
 
 @Component({
   selector: 'app-book-item',
